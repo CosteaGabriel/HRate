a031ec680821b392410cb65231cff65c8c6fc56a
Gabriel Costea <borza.paul.pbg@gmail.com>
2018-03-10 23:58:26 +0200

    Database created

---
+++ HRate/Data/HRateContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRate.Models
+{
+    public class HRateContext : DbContext
+    {
+        public HRateContext (DbContextOptions<HRateContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HRate.Models.Movie> Movie { get; set; }
+    }
+}
--- HRate/Startup.cs
+++ HRate/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using HRate.Models;
 
 namespace HRate
 {
@@ -22,6 +24,9 @@ namespace HRate
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<HRateContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("HRateContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
